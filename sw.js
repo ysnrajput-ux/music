0fe2264c9aeb7436ec61b8e80d23a31c3dc9c476
Bhawishya Kumar Sooryavanshi <akamit580@gmail.com>
2026-05-28 17:16:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-const CACHE_NAME = 'ysn-music-v1';
-
-const urlsToCache = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-
-  event.waitUntil(
-
-    caches.open(CACHE_NAME).then(cache => {
-
-      return cache.addAll(urlsToCache);
-
-    })
-
-  );
-
+self.addEventListener('install', () => {
   self.skipWaiting();
-
 });
 
 self.addEventListener('activate', event => {
@@ -30,17 +9,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
 
       return Promise.all(
-
-        cacheNames.map(cache => {
-
-          if(cache !== CACHE_NAME){
-
-            return caches.delete(cache);
-
-          }
-
-        })
-
+        cacheNames.map(cache => caches.delete(cache))
       );
 
     })
@@ -48,19 +17,5 @@ self.addEventListener('activate', event => {
   );
 
   self.clients.claim();
-
-});
-
-self.addEventListener('fetch', event => {
-
-  event.respondWith(
-
-    caches.match(event.request).then(response => {
-
-      return response || fetch(event.request);
-
-    })
-
-  );
 
 });
