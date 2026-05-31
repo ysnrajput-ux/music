316b70e4b68476866087b7655a7cfc9e3b89efa7
ysnrajput-ux <ysn.rajput@gmail.com>
2026-05-31 22:41:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let favorites = JSON.parse(
+localStorage.getItem("favorites") || "[]"
+);
 const resultsSection =
 document.getElementById("resultsSection");
 const searchInput = document.getElementById("searchInput");
@@ -11,7 +14,13 @@ const demoSongs = [
   { title: "Shape Of You", artist: "Ed Sheeran" },
   { title: "Blinding Lights", artist: "The Weeknd" }
 ];
-
+if(list.length === 0){
+songList.innerHTML = `
+<p class="text-gray-400 text-center col-span-full">
+Search music...
+</p>`;
+return;
+}
 function renderSongs(list) {
 
   songList.innerHTML = "";
